714ede68a1e66a0fb22eeb14fb44e47a1307d182
Richard Vincent <richardjohnvincent@gmail.com>
2021-10-30 10:41:01 -0400

    added data gen

--- twitter.py
+++ twitter.py
@@ -8,6 +8,9 @@ import sys
 import os
 import config
 import tweepy
+import pandas as pd
+from textblob import TextBlob
+
 
 def auth():
     key = None
@@ -19,9 +22,38 @@ def auth():
     return api
 
 def get_tweets(query, api):
+    rlist = []
     for tweet in tweepy.Cursor(api.search_tweets, q=query, lang='en').items(10):
-        print(tweet.text)
+        rlist.append(tweet.text)
+
+    return rlist
+
+def load_train_data():
+    cols=['target','ids','date','flag','user','text']
+    encoding = "ISO-8859-1"
+    df = pd.read_csv('/home/trey/movie_data/data/training/training.1600000.processed.noemoticon.csv', encoding=encoding, names=cols)
+
+    return df
+
+def data_prep(tweets):
+    data = []
+
+    for x in tweets:
+        lem_words = []
+        t = TextBlob(x.lower())
+        words = t.words
+        sen = t.sentiment.subjectivity
+        pol = t.sentiment.polarity
+        for word in words:
+            lem_words.append(word.lemmatize())
+        data.append([x, pol, sen, words, lem_words])
+
+    df = pd.DataFrame(data, columns = ['tweet', 'polarity', 'subjectivity', 'words', 'lem_words'])
+    
+    return df
 
 if __name__ == '__main__':
     api = auth()
-    get_tweets('Dune', api)
+    tweets = get_tweets('Dune', api)
+    df = data_prep(tweets)
+    print(df)
